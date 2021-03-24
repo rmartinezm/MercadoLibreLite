f82412d05cb4850c9114e8ab2f502a2bd1ce5ca6
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-03-23 22:12:07 -0600

    feat: added core module and buildsrc directory

---
+++ mercado-libre-lite-android/core/clean/src/main/java/cs/roberto/mercadolibrelite/core/clean/domain/Failure.kt
@@ -0,0 +1,7 @@
+package cs.roberto.mercadolibrelite.core.clean.domain
+
+/**
+ * Base Class for handling errors/failures/exceptions.
+ * Every feature specific failure should extend [Failure] class.
+ */
+abstract class Failure
