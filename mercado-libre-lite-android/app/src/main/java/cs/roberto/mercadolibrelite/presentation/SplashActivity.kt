783b66f25e56b77c8889f49e31a9a838f689ab07
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-03-23 23:59:14 -0600

    feat: splash design implementation

---
+++ mercado-libre-lite-android/app/src/main/java/cs/roberto/mercadolibrelite/presentation/SplashActivity.kt
@@ -0,0 +1,16 @@
+package cs.roberto.mercadolibrelite.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cs.roberto.mercadolibrelite.R
+
+/** */
+class SplashActivity : AppCompatActivity() {
+
+    /** */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
