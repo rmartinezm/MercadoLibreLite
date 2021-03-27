ebf8f044cd8ee4e5229aba4d7ffa12d3a3c4e3a3
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-03-27 12:23:23 -0600

    feat: added retrofit as http client

--- mercado-libre-lite-android/app/src/main/java/cs/roberto/mercadolibrelite/di/SharedModule.kt
+++ mercado-libre-lite-android/app/src/main/java/cs/roberto/mercadolibrelite/di/SharedModule.kt
@@ -1,11 +1,14 @@
 package cs.roberto.mercadolibrelite.di
 
+import cs.roberto.mercadolibrelite.shared_pool.httpclient.httpClientModule
 import org.koin.core.module.Module
 
 /**
  * Obtains the Modules (in terms of Koin) of tools that are required by one or more shared module.
  */
-fun getSharedPoolModules(): List<Module> = listOf()
+fun getSharedPoolModules(): List<Module> = listOf(
+    httpClientModule,
+)
 
 /**
  * Obtains the Modules (in terms of Koin) that provides user-facing functionality.
---
+++ mercado-libre-lite-android/buildSrc/src/main/kotlin/ConfigField.kt
@@ -0,0 +1,10 @@
+/* */
+object ConfigField {
+
+    /* */
+    const val API_BASE_URL: String = "API_BASE_URL"
+
+    /* */
+    const val apiBaseValue: String = "\"https://api.mercadolibre.com/sites/MLM\""
+
+}
